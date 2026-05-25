53c2e8ab41d55528e504b1a7121baa94aacbf8e6
Cristian Molano <molanocristian2006@gmail.com>
2026-05-24 21:24:11 -0500

    feat: prueba exitosa de conexion e insercion de datos en BD

--- src/com/peliculas/main/Main.java
+++ src/com/peliculas/main/Main.java
@@ -1,11 +1,21 @@
 package com.peliculas.main;
 
-import com.peliculas.connection.DatabaseConnection;
-import java.sql.Connection;
+import com.peliculas.dao.DirectorDAO;
+import com.peliculas.model.Director;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Iniciando prueba de conexión...");
-        Connection conexion = DatabaseConnection.getConnection();
+        System.out.println("Iniciando el programa...");
+
+        // 1. Creamos un objeto de tipo Director con datos reales
+        Director nuevoDirector = new Director();
+        nuevoDirector.setNombre("Christopher Nolan"); // Puedes poner el director que quieras
+        nuevoDirector.setPais("Reino Unido");
+
+        // 2. Usamos nuestro DAO para enviar ese director a la base de datos Neon
+        DirectorDAO directorDAO = new DirectorDAO();
+        directorDAO.agregarDirector(nuevoDirector);
+
+        System.out.println("Proceso finalizado.");
     }
 }
